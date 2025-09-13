2d3e82f3e6433017ced13062dfdefeafdbd264d5
cc-kamsiii <camero.christian@dfcamclp.edu.ph>
2025-09-13 09:30:12 +0800

    log in and owner and staff

--- frontend/src/components/Sidebar/OwnerSidebar.jsx
+++ frontend/src/components/Sidebar/OwnerSidebar.jsx
@@ -5,9 +5,19 @@ import "../../Style/Sidebar.css";
 function OwnerSidebar({ name, onLogout }) {
   return (
     <div className="sidebar">
-      <h2>POS System</h2>
+
+
+      {/* 
       <p style={{ fontSize: "14px", color: "lightgray" }}>Name: {name}</p>
-      <p style={{ fontSize: "12px", color: "lightgray" }}>Role: Owner</p>
+      <p style={{ fontSize: "12px", color: "lightgray" }}>Role: Owner</p>*/
+      }
+
+      <div className="nameAndRole">
+        <div className="b-border">
+          <p>Role: Owner</p>
+          <p>{name}</p>
+        </div>
+      </div>
 
       <ul>
         <li><Link to="/dashboard">Dashboard</Link></li>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,62 +1,69 @@
 import "../Style/Login.css";
-import logo from "../assets/logo.jpg";
-import { useState} from "react";
-import axios from 'axios';
+import logo from "../assets/logo.jpg"; 
+import sisig from "../assets/sisig.jpg"
+import { useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-
-
-  const [username, setUsername] = useState("")
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:8081/auth/login', {username, password});
+      const res = await axios.post("http://localhost:8081/auth/login", {
+        username,
+        password,
+      });
 
-      if(res.data.success){
+      if (res.data.success) {
         localStorage.setItem("token", res.data.token);
-        localStorage.setItem("name", res.data.name)
-        localStorage.setItem("role", res.data.role); 
-        navigate("/Dashboard")
-      }
-      else{
+        localStorage.setItem("name", res.data.name);
+        localStorage.setItem("role", res.data.role);
+        navigate("/Dashboard");
+      } else {
         alert(res.data.message);
       }
     } catch (err) {
-        console.log(err);
-        alert("SERVER ERROR");
+      console.log(err);
+      alert("SERVER ERROR");
     }
-  }
+  };
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <img src={logo} width="200" className="logo" />
-        <div className="head">
-          <h1>Login</h1>
-        </div>
+    <div className="login-container">
+      <div className="login-left">
+        <img src={logo} alt="Logo" className="login-logo" />
+        <h2>P.O.S and Inventory Management System</h2>
+      </div>
 
-        <div className="inputs">
-            <div className="input">
-              <input type="text" placeholder="Username" required onChange={(e) => setUsername(e.target.value)} />
-            </div>
+      <div className="login-right">
+        <form onSubmit={handleSubmit} className="login-form">
+          <h3 className="login-title">Log in</h3>
+          <p>Enter your credentials to continue</p>
 
-            <div className="input">
-              <input type="password" placeholder="Password" required  onChange={(e) => setPassword(e.target.value)}/>
-            </div>
-            
-        </div>
+          <input
+            type="text"
+            placeholder="Email address"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-        <div className="Sub-Cont">
-          <button type="submit" className="Submit">Login</button>
-        </div>
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-      </form>
-
+          <button type="submit" className="login-btn">
+            Log in
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
