9accd67512fe63e3a764ed9bb692a59f8dd263f4
cc-kamsiii <camero.christian@dfcamclp.edu.ph>
2025-10-28 17:11:32 +0800

    may binago lang ulit, commit ulit

--- frontend/src/pages/UserNotLoggedIn.jsx
+++ frontend/src/pages/UserNotLoggedIn.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
+import exclamation from "../assets/exclamation.png";
+import "../Style/NotLoggedIn.css";
 
-const UserNotLoggedIn = ({ isVisible, onClose}) => {
+const UserNotLoggedIn = ({ isVisible, onClose }) => {
   if (!isVisible) return null;
 
   return (
     <div className="modal-overlay">
       <div className="modal-content">
         <div className="modal-body">
-          <h3 className="success-title">User not found</h3>
-          <button onClick={onClose} style={{color:"white", backgroundColor:"red", border:"none", padding: "0.75rem 2rem", borderRadius: "6px", cursor:"pointer", fontWeight:"500"}}>
+          <img src={exclamation} alt="Logo" className="exclamation" />
+          <h3 className="user-not-found">User not found</h3>
+          <button onClick={onClose} className="close-not-found">
             Close
           </button>
         </div>
