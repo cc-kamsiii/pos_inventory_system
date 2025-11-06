05b60811a341bb7ade8228e6227f381f76b4ba22
cc-kamsiii <camero.christian@dfcamclp.edu.ph>
2025-11-06 12:36:40 +0800

    Display Staff Login Time on Main Landing Page

--- backend/routes/ownerTransactions.js
+++ backend/routes/ownerTransactions.js
@@ -1,14 +1,22 @@
 import express from "express";
-import {getTransactions, addTransactions, getTotalSales, getTotalSalesBreakdown, getOrdersSummary, getSalesChart} from "../controllers/ownerTransactionsControllers.js";
+import {
+  getTransactions,
+  addTransactions,
+  getTotalSales,
+  getTotalSalesBreakdown,
+  getOrdersSummary,
+  getSalesChart,
+  getCashierLogins
+} from "../controllers/ownerTransactionsControllers.js";
 
 const router = express.Router();
 
-router.get('/', getTransactions);
-router.post('/', addTransactions);
-router.get('/total_sales', getTotalSales)
-router.get('/total_sales_breakdown', getTotalSalesBreakdown);
-router.get('/orders_summary', getOrdersSummary);
-router.get('/sales_chart', getSalesChart);
+router.get("/", getTransactions);
+router.post("/", addTransactions);
+router.get("/total_sales", getTotalSales);
+router.get("/total_sales_breakdown", getTotalSalesBreakdown);
+router.get("/orders_summary", getOrdersSummary);
+router.get("/sales_chart", getSalesChart);
+router.get("/cashier_logins", getCashierLogins);
 
 export default router;
-
